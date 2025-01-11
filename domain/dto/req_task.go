bc401b23d85705dd0064a3a286f43910c8bacd7b
putra <putra@web8.is>
2025-01-12 02:33:39 +0700

    chore: add categroy id

--- domain/dto/req_task.go
+++ domain/dto/req_task.go
@@ -3,6 +3,7 @@ package dto
 type ReqTask struct {
 	Title       string   `json:"title" bson:"title"`
 	Description string   `json:"description" bson:"description"`
+	CategoryId  string   `json:"category_id" bson:"category_id"`
 	Status      string   `json:"status" bson:"status"`
 	Priority    string   `json:"priority" bson:"priority"`
 	Tags        []string `json:"tags" bson:"tags"`
